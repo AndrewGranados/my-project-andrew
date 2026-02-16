68f5635062c0f8fa788dfba804b8f1dac280176f
Andrew <aleks.garcia.009@gmail.com>
2026-02-16 00:26:52 -0600

    se modificó el api/employees/id/route debido a un error de promise en el id

--- app/api/employees/[id]/route.ts
+++ app/api/employees/[id]/route.ts
@@ -1,3 +1,4 @@
+/*
 import { NextResponse } from "next/server";
 import { sql } from "@/lib/db";
 //import { redirect } from "next/navigation";
@@ -63,6 +64,81 @@ export async function DELETE(
   await sql`
     DELETE FROM Employee
     WHERE id = ${params.id}
+  `;
+
+  return NextResponse.json({ ok: true });
+}
+*/
+
+import { NextResponse } from "next/server";
+import { sql } from "@/lib/db";
+
+type Params = {
+  params: Promise<{ id: string }>;
+};
+
+export async function GET(
+  req: Request,
+  { params }: Params
+) {
+  const { id } = await params;
+
+  const [employee] = await sql`
+    SELECT *
+    FROM "Employee"
+    WHERE id = ${id}
+  `;
+
+  if (!employee) {
+    return NextResponse.json(
+      { message: "Empleado no encontrado" },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(employee);
+}
+
+export async function PUT(
+  req: Request,
+  { params }: Params
+) {
+  const { id } = await params;
+
+  const body = await req.json();
+
+  const {
+    firstName,
+    lastName,
+    salary,
+    phone,
+    email
+  } = body;
+
+  const [employee] = await sql`
+    UPDATE "Employee"
+    SET
+      "firstName" = ${firstName},
+      "lastName"  = ${lastName},
+      salary      = ${salary},
+      phone       = ${phone},
+      email       = ${email}
+    WHERE id = ${id}
+    RETURNING *
+  `;
+
+  return NextResponse.json(employee);
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: Params
+) {
+  const { id } = await params;
+
+  await sql`
+    DELETE FROM "Employee"
+    WHERE id = ${id}
   `;
 
   return NextResponse.json({ ok: true });
